e004ddd2d0d2c8241518fc423758ac1bb55befab
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2022-06-21 15:00:30 -0300

    Finaliza projeto webpack

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const Webpack = require('webpack');
+
+module.exports = {
+    entry: './app/src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'app/dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                // test:/\.css$/, use: ['style-loader','css-loader']
+                test:/\.css$/, use: [MiniCssExtractPlugin.loader,'css-loader']
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerWebpackPlugin(),
+            '...'
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: './app/src/index.html',
+        filename: 'index.html',
+        hash: true
+    }),
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         {
+        //             from: './app/src/css', 
+        //             to: 'css'
+        //         }
+        //     ]
+        // })
+
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new Webpack.optimize.ModuleConcatenationPlugin()
+    ],
+    devServer: {
+        // contentBase: path.resolve(__dirname, 'dist'),
+        // port: 3000
+        static: {
+            directory: path.join(__dirname, "dist")
+          },
+          compress: true,
+          port: 3000, // default 8000
+    }
+}
